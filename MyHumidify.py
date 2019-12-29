173a4ccffcf07175b99292c25f6e1a4af65231dd
Lukasz Antkowiak <lukasz.patryk.antkowiak@gmail.com>
2019-12-30 00:48:08 +0100

    quick fix

--- MyHumidify.py
+++ MyHumidify.py
@@ -8,43 +8,43 @@ path=site.getsitepackages()
 for i in path:
     sys.path.append(i)
 
-import miio.airhumidifier
+import miio.airpurifier
 
-parser = argparse.ArgumentParser(description='Script which comunicate with AirHumidifier.')
-parser.add_argument('IPaddress', help='IP address of AirHumidifier' )
+parser = argparse.ArgumentParser(description='Script which comunicate with AirPurfier.')
+parser.add_argument('IPaddress', help='IP address of AirPurfier' )
 parser.add_argument('token', help='token to login to device')
 parser.add_argument('--mode', choices=['Auto', 'Favorite', 'Idle', 'Silent'], help='choose mode operation')
 parser.add_argument('--favoriteLevel', type=int, choices=range(0, 11), help='choose mode operation')
 parser.add_argument('--power', choices=['ON', 'OFF'], help='power ON/OFF')
 parser.add_argument('--debug', action='store_true', help='if define more output is printed')
 
-# MyHumidify.set_mode(miio.airhumidifier.OperationMode.Silent)
+# MyAir.set_mode(miio.airpurifier.OperationMode.Silent)
 
 args = parser.parse_args()
 if args.debug:
     print(args)
-MyHumidify = miio.airhumidifier.AirHumidifierCA1(args.IPaddress, args.token)
+MyAir = miio.airpurifier.AirPurifier(args.IPaddress, args.token)
 
 if args.mode:
     if args.mode == "Auto":
-            MyHumidify.set_mode(miio.airhumidifier.OperationMode.Auto)
+            MyAir.set_mode(miio.airpurifier.OperationMode.Auto)
     elif args.mode == "Favorite":
-            MyHumidify.set_mode(miio.airhumidifier.OperationMode.Favorite)
+            MyAir.set_mode(miio.airpurifier.OperationMode.Favorite)
     elif args.mode == "Idle":
-            MyHumidify.set_mode(miio.airhumidifier.OperationMode.Idle)
+            MyAir.set_mode(miio.airpurifier.OperationMode.Idle)
     elif args.mode == "Silent":
-            MyHumidify.set_mode(miio.airhumidifier.OperationMode.Silent)
+            MyAir.set_mode(miio.airpurifier.OperationMode.Silent)
 
 if args.favoriteLevel:
-    MyHumidify.set_favorite_level(args.favoriteLevel)
+    MyAir.set_favorite_level(args.favoriteLevel)
 
 if args.favoriteLevel:
-    MyHumidify.set_favorite_level(args.favoriteLevel)
+    MyAir.set_favorite_level(args.favoriteLevel)
 
 if args.power:
     if args.power == "ON":
-        MyHumidify.on()
+        MyAir.on()
     elif args.power == "OFF":
-        MyHumidify.off()
+        MyAir.off()
 
-print(MyHumidify.status())
+print(MyAir.status())
