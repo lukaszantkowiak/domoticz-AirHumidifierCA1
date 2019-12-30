ee18aee9549bae7841dc0621f34501a9301094df
Lukasz Antkowiak <lukasz.patryk.antkowiak@gmail.com>
2019-12-30 03:01:08 +0100

    quick fix

--- MyHumidify.py
+++ MyHumidify.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
 
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile1.txt", "w")
-file.write("a")
-file.close()
-
 import sys
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile2.txt", "w")
-file.write("a")
-file.close()
-
 import argparse
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile3.txt", "w")
-file.write("a")
-file.close()
-
 import site
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile4.txt", "w")
-file.write("a")
-file.close()
 
 path=''
 path=site.getsitepackages()
 
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile5.txt", "w")
-file.write("a")
-file.close()
-
 for i in path:
     sys.path.append(i)
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile6.txt", "w")
-file.write("a")
-file.close()
 
 import miio
 import miio.airhumidifier
 import miio.purifier
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile7.txt", "w")
-file.write("a")
-file.close()
-
 
 parser = argparse.ArgumentParser(description='Script which comunicate with AirPurfier.')
 parser.add_argument('IPaddress', help='IP address of AirPurfier' )
@@ -53,18 +22,9 @@ parser.add_argument('--favoriteLevel', type=int, choices=range(0, 11), help='cho
 parser.add_argument('--power', choices=['ON', 'OFF'], help='power ON/OFF')
 parser.add_argument('--debug', action='store_true', help='if define more output is printed')
 
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile8.txt", "w")
-file.write("a")
-file.close()
-
 # MyAir.set_mode(miio.airhumidifier.OperationMode.Silent)
 
 args = parser.parse_args()
-
-file = open("/home/pi/domoticz/plugins/domoticz-AirHumidifierCA1/testfile9.txt", "w")
-file.write("a")
-file.close()
 
 if args.debug:
     print(args)
@@ -76,8 +36,6 @@ file.write(str(dir(miio)) + "\n")
 file.write(str(dir(miio.airhumidifier)) + "\n")
 file.write(str(dir(miio.purifier)) + "\n")
 file.close()
-
-
 
 try:
     MyAir = miio.airhumidifier.AirHumidifierCA1(args.IPaddress, args.token)
